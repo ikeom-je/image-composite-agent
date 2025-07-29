7f47280fe2758fd75fc4ec5a7b5e9c7946e8422e
ikeomasaya <ikeomje@gmail.com>
2025-07-29 22:26:54 +0900

    fix embed params to code

--- lib/image-processor-api-stack.ts
+++ lib/image-processor-api-stack.ts
@@ -2,12 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as path from 'path';
 import { Construct } from 'constructs';
 
 export class ImageProcessorApiStack extends cdk.Stack {
   public readonly resourcesBucket: s3.Bucket;
   public readonly testImagesBucket: s3.Bucket;
+  public readonly frontendBucket: s3.Bucket;
+  public readonly distribution: cloudfront.Distribution;
   public readonly apiEndpoint: string;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -27,6 +33,15 @@ export class ImageProcessorApiStack extends cdk.Stack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // S3バケットの作成（フロントエンド用）
+    this.frontendBucket = new s3.Bucket(this, 'FrontendBucket', {
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
     // Python Lambda関数の作成（シンプルなバンドリング）
@@ -85,10 +100,87 @@ export class ImageProcessorApiStack extends cdk.Stack {
     // APIエンドポイントを保存
     this.apiEndpoint = `${api.url}images/composite`;
 
+    // CloudFront Origin Access Identity (OAI) の作成
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity', {
+      comment: 'Access to the frontend bucket',
+    });
+
+    // S3バケットポリシーの設定 - CloudFrontからのアクセスのみを許可
+    this.frontendBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        effect: iam.Effect.ALLOW,
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+        resources: [this.frontendBucket.arnForObjects('*')],
+      })
+    );
+
+    // CloudFrontディストリビューションの作成
+    this.distribution = new cloudfront.Distribution(this, 'FrontendDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(this.frontendBucket, {
+          originAccessIdentity,
+        }),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      // SPAのルーティングをサポートするためのエラーレスポンス設定
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(0),
+        },
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100, // コスト最適化
+    });
+
+    // 設定ファイルの内容を動的生成
+    const configContent = {
+      apiUrl: this.apiEndpoint,
+      cloudfrontUrl: `https://${this.distribution.distributionDomainName}`,
+      s3BucketNames: {
+        resources: this.resourcesBucket.bucketName,
+        testImages: this.testImagesBucket.bucketName,
+        frontend: this.frontendBucket.bucketName,
+      },
+      version: process.env.npm_package_version || '2.1.0',
+      environment: this.node.tryGetContext('environment') || 'production',
+      features: {
+        enableS3Upload: this.node.tryGetContext('enableS3Upload') === 'true',
+        enableAdvancedFilters: this.node.tryGetContext('enableAdvancedFilters') === 'true',
+      }
+    };
+
+    // フロントエンドと設定ファイルを同時にデプロイ
+    new s3deploy.BucketDeployment(this, 'DeployFrontendWithConfig', {
+      sources: [
+        s3deploy.Source.asset(path.join(__dirname, '../frontend/dist')),
+        s3deploy.Source.jsonData('config.json', configContent)
+      ],
+      destinationBucket: this.frontendBucket,
+      distribution: this.distribution,
+      distributionPaths: ['/*'], // CloudFrontキャッシュを無効化
+      retainOnDelete: false,
+    });
+
     // 出力値の設定
     new cdk.CfnOutput(this, 'ApiUrl', {
       value: this.apiEndpoint,
       description: 'URL for the Image Processor API'
+    });
+
+    new cdk.CfnOutput(this, 'FrontendUrl', {
+      value: `https://${this.distribution.distributionDomainName}`,
+      description: 'URL for the frontend application'
     });
 
     new cdk.CfnOutput(this, 'TestImagesBucketName', {
@@ -99,6 +191,11 @@ export class ImageProcessorApiStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'ResourcesBucketName', {
       value: this.resourcesBucket.bucketName,
       description: 'Name of the resources bucket'
+    });
+
+    new cdk.CfnOutput(this, 'FrontendBucketName', {
+      value: this.frontendBucket.bucketName,
+      description: 'Name of the frontend bucket'
     });
   }
 }
